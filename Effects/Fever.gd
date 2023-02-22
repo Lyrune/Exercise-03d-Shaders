22cb4a745d6454768828eade44dcdad2e5ccf87b
Lyrune <122567031+Lyrune@users.noreply.github.com>
2023-02-21 23:48:04 -0500

    need to port to desktop PC

--- Effects/Fever.gd
+++ Effects/Fever.gd
@@ -1,13 +1,21 @@
 extends Node2D
 
 func start_fever():
+	print("Fever Started!")
 	fever()
 	$Timer.start()
+	var Fever_indicator = get_node_or_null("/root/Game/UI/HUD/Fever")
+	if Fever_indicator != null:
+		Fever_indicator.use_parent_material = false
 
 func _on_Timer_timeout():
 	if Global.feverish:
 		fever()
 		$Timer.start()
+	else:
+		var Fever_indicator = get_node_or_null("/root/Game/UI/HUD/Fever")
+		if Fever_indicator != null:
+			Fever_indicator.use_parent_material = true
 
 func fever():
 	var ball_container = get_node_or_null("/root/Game/Ball_Container")
